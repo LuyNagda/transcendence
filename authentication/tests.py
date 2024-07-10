20b8ee68fe166a44b2c9e63ea21d002c8167c47b
Luy Nagda <luy2709@yahoo.com>
2024-07-10 22:02:40 +0200

    Removing redundant tests

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,28 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest, HttpResponse
-from .middleware import HTMXMiddleware
 from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
-
-class HTMXMiddlewareTest(TestCase):
-    def setUp(self):
-        def get_response(request):
-            return HttpResponse("Dummy response")
-        self.middleware = HTMXMiddleware(get_response)
-
-    def test_htmx_request_allowed(self):
-        request = HttpRequest()
-        request.path = '/login/'
-        request.htmx = True
-        response = self.middleware(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_non_htmx_request_forbidden(self):
-        request = HttpRequest()
-        request.path = '/login/'
-        request.htmx = False
-        response = self.middleware(request)
-        self.assertEqual(response.status_code, 403)
         
 class CustomUserCreationFormTest(TestCase):
     def test_form_valid(self):
