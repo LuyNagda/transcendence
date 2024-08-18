6d0b1e50b2eb0bc372898d40a83646100e5b52c8
jmoutous <julien.moutoussamy@gmail.com>
2024-08-18 14:27:29 +0200

    UPDATE: only use one layer of 3 neurons

--- AI/NNAI.py
+++ AI/NNAI.py
@@ -5,7 +5,7 @@ class Neuron_Network:
     def __init__(self):
         self.layer1 = Layer_Dense(5, 3)
         self.activation1 = Activation_ReLU()
-        self.layer2 = Layer_Dense(3, 3)
+        # self.layer2 = Layer_Dense(3, 3)
         self.activation2 = Activation_SoftMax()
         self.ai_score = 0
 
@@ -18,13 +18,19 @@ class Neuron_Network:
 def AI_neurons(ai, X):
     ai.layer1.forward(X)
     ai.activation1.forward(ai.layer1.output)
-    ai.layer2.forward(ai.activation1.output)
-    ai.activation2.forward(ai.layer2.output)
 
-    return np.argmax(ai.activation2.output)
+    # ai.layer2.forward(ai.activation1.output)
+    # ai.activation2.forward(ai.layer2.output)
 
-def AI_decision(ai, opponent, opponent_ball, HEIGHT):
-    X = [opponent_ball.x / HEIGHT, opponent_ball.y / HEIGHT, opponent_ball.dx, opponent_ball.dy, opponent.y]
+    # return np.argmax(ai.activation2.output)
+
+    ai.activation2.forward(ai.activation1.output)
+    ai_response = np.argmax(ai.activation2.output)
+
+    return ai_response
+
+def AI_decision(ai, opponent, ai_ball, HEIGHT):
+    X = [ai_ball.x / HEIGHT, ai_ball.y / HEIGHT, ai_ball.dx, ai_ball.dy, opponent.y]
     return AI_neurons(ai, X)
 
     
