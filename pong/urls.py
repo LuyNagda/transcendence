c98cd2843365ddf255190aa2a1279f3b124f65e8
ZEDIUM-Off <matisse.chenavas@gmail.com>
2024-09-12 16:28:55 +0200

    wip pong room/game managment

--- pong/urls.py
+++ pong/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.pong_view, name='pong_view'),
-    path('create/', views.create_pong_room, name='create_pong_room'),
-    path('update/<int:game_id>/', views.update_pong_game_state, name='update_pong_game_state'),
-    path('state/<int:game_id>/', views.get_pong_game_state, name='get_pong_game_state'),
     path('history/', views.game_history, name='game_history'),
+    path('create-room/', views.create_pong_room, name='create_pong_room'),
+    path('room/<str:room_id>/', views.pong_room_view, name='pong_room'),
+    path('room/<str:room_id>/invite-friend/', views.invite_friend, name='invite_friend'),
+    path('room/<str:room_id>/invite-friends/', views.invite_friends, name='invite_friends'),
+    path('room/<str:room_id>/start-ai-game/', views.start_ai_game, name='start_ai_game'),
+    path('game/<int:game_id>/update/', views.update_pong_game_state, name='update_pong_game_state'),
+    path('game/<int:game_id>/', views.pong_game, name='pong_game'),
 ]
--- pong/views.py
+++ pong/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import PongGame, PongRoom
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from authentication.decorators import IsAuthenticatedWithCookie
 from django.db import models
 import uuid
+import logging
 
 User = get_user_model()
+
+# Configure the logger
+logger = logging.getLogger(__name__)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedWithCookie])
@@ -18,7 +21,7 @@ def pong_view(request):
     refresh_token = request.COOKIES.get('refresh_token')
     users = User.objects.exclude(email=request.user.email)
     return render(request, 'pong/pong.html', {
-        'users': users, 
+        'users': users,
         'access_token': access_token, 
         'refresh_token': refresh_token
     })
@@ -42,7 +45,7 @@ def create_pong_room(request):
     room_id = str(uuid.uuid4())[:8]
     room = PongRoom.objects.create(room_id=room_id)
     room.players.set(players)
-    return JsonResponse({'room_id': room.room_id, 'status': 'created', 'access_token': access_token, 'refresh_token': refresh_token})
+    return redirect('pong_room', room_id=room.room_id)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticatedWithCookie])
@@ -55,16 +58,76 @@ def update_pong_game_state(request, game_id):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedWithCookie])
-def get_pong_game_state(request, game_id):
+def pong_game(request, game_id):
     access_token = request.COOKIES.get('access_token')
     refresh_token = request.COOKIES.get('refresh_token')
     game = get_object_or_404(PongGame, id=game_id)
-    return JsonResponse({
-        'player1': game.player1.username,
-        'player2': game.player2.username,
-        'score_player1': game.score_player1,
-        'score_player2': game.score_player2,
-        'is_active': game.is_active,
+    game_state = game.get_state()
+    return render(request, 'pong/game.html', {
+        'game': game_state,
         'access_token': access_token,
         'refresh_token': refresh_token
     })
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedWithCookie])
+def pong_room_view(request, room_id):
+    access_token = request.COOKIES.get('access_token')
+    refresh_token = request.COOKIES.get('refresh_token')
+    room = get_object_or_404(PongRoom, room_id=room_id)
+    
+    if request.method == 'POST':
+        mode = request.POST.get('mode')
+        room.mode = mode
+        room.save()
+        if mode == PongRoom.Mode.AI:
+            # Logique pour jouer contre l'AI
+            pass
+        elif mode == PongRoom.Mode.CLASSIC:
+            # Logique pour inviter un ami ou lancer un matchmaking
+            pass
+        elif mode == PongRoom.Mode.RANKED:
+            # Logique pour lancer un matchmaking
+            pass
+        elif mode == PongRoom.Mode.TOURNAMENT:
+            # Logique pour inviter des amis ou lancer un matchmaking
+            pass
+        return redirect('pong_room', room_id=room.room_id)
+    
+    return render(request, 'pong/pong-room.html', {
+        'room': room,
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    })
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticatedWithCookie])
+def invite_friend(request, room_id):
+    access_token = request.COOKIES.get('access_token')
+    refresh_token = request.COOKIES.get('refresh_token')
+    room = get_object_or_404(PongRoom, room_id=room_id)
+    friend_id = request.POST.get('friend')
+    friend = get_object_or_404(User, id=friend_id)
+    room.players.add(friend)
+    return redirect('pong_room', room_id=room.room_id)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticatedWithCookie])
+def invite_friends(request, room_id):
+    access_token = request.COOKIES.get('access_token')
+    refresh_token = request.COOKIES.get('refresh_token')
+    room = get_object_or_404(PongRoom, room_id=room_id)
+    friend_ids = request.POST.getlist('friends')
+    friends = User.objects.filter(id__in=friend_ids)
+    room.players.add(*friends)
+    return redirect('pong_room', room_id=room_id)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticatedWithCookie])
+def start_ai_game(request, room_id):
+    logger.info(f"Requête reçue pour démarrer une partie AI dans la salle {room_id} par l'utilisateur {request.user.username}")
+    room = get_object_or_404(PongRoom, room_id=room_id)
+    player1 = request.user
+    game = PongGame.objects.create(room=room, player1=player1, player2_is_ai=True, status=PongGame.Status.ONGOING)
+    logger.info(f"Partie AI créée avec succès. ID de la partie : {game.id}")
+    return redirect('pong_game', game_id=game.id)
