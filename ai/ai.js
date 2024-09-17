988367df3f960af768010b39803d9cec8198d8a9
jmoutous <julien.moutoussamy@gmail.com>
2024-09-17 15:05:55 +0200

    UPDATE: prototype ai js

--- ai/ai.js
+++ ai/ai.js
@@ -1,19 +1,13 @@
-function dot(weights, inputs, biases, output) {
-    let x = weights.length
-    let y = weights[0].length
-
-    for (var i = 0; i < x; ++i) {
-        // output[i] = 
-    }
-
-}
-
 class Layer_Dense {
     constructor(weights) {
         this.weights = weights
     }
 
     forward(inputs){
-        // this.output = 
+        this.output = this.matrixAdd()
     }
+
+    matrixAdd() {
+
+    };
 }
