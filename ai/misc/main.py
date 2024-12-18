aedb3068b4a10337caaca5c508a14b3411d94276
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2024-12-18 13:02:55 +0100

    FIX: main.py to train without server

--- ai/misc/main.py
+++ ai/misc/main.py
@@ -8,13 +8,10 @@ sys.path.insert(0, project_root)
 from ai.ai import train_ai
 
 def main():
-    print("sys.argv[0]:", sys.argv[0])
-    print("sys.argv[1]:", sys.argv[1])
-    print("sys.argv[2]:", sys.argv[2])
-    if (len(sys.argv) != 3):
+    if (len(sys.argv) != 2):
         return print("Please provide a saved AI!")
     
-    save_file = sys.argv[2]
+    save_file = sys.argv[1]
     train_ai(save_file)
 
 if __name__ == "__main__":
