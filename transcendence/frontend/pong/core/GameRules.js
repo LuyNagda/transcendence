73f65ddb75c9d5d1e52ca7b5f07b633ff9b53be3
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2025-01-07 14:51:39 +0100

    UPDATE: Add static PADDLE_HEIGHT to the GameRules.js

--- transcendence/frontend/pong/core/GameRules.js
+++ transcendence/frontend/pong/core/GameRules.js
@@ -1,7 +1,8 @@
 export class GameRules {
 	static WINNING_SCORE = 11;
 	static BALL_SPEED = 107;
-	static PADDLE_SPEED = 12;
+	static PADDLE_SPEED = 15;
+	static PADDLE_HEIGHT = 15;
 	static INITIAL_BALL_SPEED = 2;
 	static CANVAS_WIDTH = 858;
 	static CANVAS_HEIGHT = 525;
