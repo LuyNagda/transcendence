25e9a3dad45321ef1fb9bcdab193a1647aa0c1fd
Luy Nagda <luy2709@yahoo.com>
2025-01-23 23:51:52 +0530

    Add a function to calculate player ranking

--- pong/tournament_functions.py
+++ pong/tournament_functions.py
@@ -1,6 +1,7 @@
 import random
 from typing import List, Tuple, Union
 from authentication.models import User
+from pong.models import Tournament, Match
 
 def randomize_and_pair_players(players: List[User]) -> List[Union[Tuple[User, User], Tuple[User, None]]]:
     """
@@ -16,3 +17,32 @@ def randomize_and_pair_players(players: List[User]) -> List[Union[Tuple[User, Us
     pairs = [(players[i], players[i + 1] if i + 1 < len(players) else None) for i in range(0, len(players), 2)]
 
     return pairs
+
+def calculate_rankings(tournament: Tournament) -> List[Tuple[User, dict]]:
+    """
+    Calculates the player rankings for a given tournament based on completed matches.
+
+    Returns a list of tuples where each tuple contains a player and their stats.
+    """
+    rankings = {}
+    for game in Match.objects.filter(tournament=tournament):
+        if game.completed_at:
+            if game.pong_game.player1:
+                if game.pong_game.player1 not in rankings:
+                    rankings[game.pong_game.player1] = {'wins': 0, 'losses': 0, 'points': 0}
+            if game.pong_game.player2:
+                if game.pong_game.player2 not in rankings:
+                    rankings[game.pong_game.player2] = {'wins': 0, 'losses': 0, 'points': 0}
+            
+            if game.pong_game.player1_score > game.pong_game.player2_score:
+                rankings[game.pong_game.player1]['wins'] += 1
+                rankings[game.pong_game.player1]['points'] += 3  # 3 points for a win
+                rankings[game.pong_game.player2]['losses'] += 1
+            else:
+                rankings[game.pong_game.player2]['wins'] += 1
+                rankings[game.pong_game.player2]['points'] += 3  # 3 points for a win
+                rankings[game.pong_game.player1]['losses'] += 1
+
+        # Sort players by points, then by wins
+        sorted_rankings = sorted(rankings.items(), key=lambda item: (item[1]['points'], item[1]['wins']), reverse=True)
+        return sorted_rankings
