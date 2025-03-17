1f293ad1a383d1de61ad5f8a4037b1305f49dbd7
agaley <agaley@student.42lyon.fr>
2025-03-17 05:44:00 +0100

    [Room] Remove useless views

--- pong/urls.py
+++ pong/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('room/create/', views.create_pong_room, name='create_pong_room'),
     path('room/<str:room_id>/', views.pong_room_view, name='pong_room'),
     path('room/<str:room_id>/state/', views.pong_room_state, name='pong_room_state'),
-    path('room/<str:room_id>/invite/', views.invite_friend, name='invite_friend'),
     path('room/<str:room_id>/invite_friends/', views.invite_friends, name='invite_friends'),
     path('game/<str:game_id>/', views.pong_game, name='pong_game'),
 ]
