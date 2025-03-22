25e922eb4d847b6057658f95f53da6822591a173
Luy Nagda <luy2709@yahoo.com>
2025-03-22 16:36:47 +0530

    Fix: JWT limits

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -105,8 +105,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
