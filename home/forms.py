7aec9e2af94c2101bfa22e651314e9cae5929dd1
gogolescargot <gauthiergalon@pm.me>
2025-03-28 01:30:27 +0100

    Revamped Templating, Removed Bio

--- home/forms.py
+++ home/forms.py
@@ -9,7 +9,6 @@ class ProfileForm(forms.Form):
     nick_name = forms.CharField(max_length=10, label='Tournament Alias', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'name', 'autocomplete': 'nickname'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email', 'autocomplete': 'email'}))
     date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'placeholder': 'YYYY-MM-DD'}), input_formats=['%Y-%m-%d'], required=False)
-    bio = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'bio'}), required=False)
     profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control', 'placeholder': 'profile-picture'}), required=False)
 
     def __init__(self, *args, **kwargs):
@@ -21,7 +20,6 @@ class ProfileForm(forms.Form):
         self.fields['nick_name'].initial = user.nick_name 
         self.fields['email'].initial = user.email
         self.fields['date_of_birth'].initial = user.date_of_birth
-        self.fields['bio'].initial = user.bio
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -36,12 +34,6 @@ class ProfileForm(forms.Form):
         if dob and dob > timezone.now().date() - timezone.timedelta(days=365 * 16):
             raise forms.ValidationError('Date of birth cannot be less than 16 years')
         return dob
-
-    def clean_bio(self):
-        bio = self.cleaned_data.get('bio')
-        if bio and len(bio) > 500:
-            raise forms.ValidationError('Bio cannot exceed 500 characters.')
-        return bio
 
     def clean_name(self):
         name = self.cleaned_data.get('name')
@@ -72,7 +64,6 @@ class ProfileForm(forms.Form):
         self.instance.email = self.cleaned_data['email']
         self.instance.nick_name = self.cleaned_data['nick_name']
         self.instance.date_of_birth = self.cleaned_data['date_of_birth']
-        self.instance.bio = self.cleaned_data['bio']
 
         if self.cleaned_data.get('profile_picture'):
             self.instance.profile_picture = self.cleaned_data['profile_picture']
